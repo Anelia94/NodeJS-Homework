23651826f36c60d61e934a1eeb40ac85232b5968
Anelia Radeva <nellyy.radeva@gmail.com>
2022-01-25 15:28:40 +0200

    Added sendEmail function

--- Task1/app.js
+++ Task1/app.js
@@ -3,49 +3,81 @@ const URL = 'http://api.weatherapi.com/v1';
 const FILE_NAME = '/current.json';
 fs = require('fs');
 http = require('http');
+nodemailer = require('nodemailer');
 
-const cityName = getCityNameFromFile('./config.json');
+const cityName = getValueFromJSONFile('city', './config.json');
+
 const fileDescriptor = fs.openSync("output.txt", "r+");
 
 const currentWeatherfullUrl = `${URL}${FILE_NAME}?key=${API_KEY}&q=${cityName}`;
-http.get(currentWeatherfullUrl, (res) => {
 
+http.get(currentWeatherfullUrl, (res) => {
     let data = "";
     res.on("data", (chunk) => {
         // Data is being received in chunks, we add it to the data variable to save it
         data += chunk;
     })
-    res.on("end", () => {
-        // all data has been received, now we can parse it and are done
-        const parsedData = JSON.parse(data);
-        const localtime = parsedData.location.localtime;
-        const weather = parsedData.current.temp_c;
+        ; res.on("end", () => {
+            // all data has been received, now we can parse it and are done
+            const parsedData = JSON.parse(data);
+            const localtime = parsedData.location.localtime;
+            const weather = parsedData.current.temp_c;
 
-        const message = getInformationAboutLocalTimeAndWeather(localtime, weather);
-        fs.writeSync(fileDescriptor, message);
+            const message = getInformationAboutLocalTimeAndWeather(localtime, weather);
+            fs.writeSync(fileDescriptor, message);
 
-        const jsonMessage = JSON.stringify(message);
-        console.log(message);
+            const jsonMessage = JSON.stringify(message);
+            console.log(message);
 
-        http.createServer(function (req, res) {
-            res.write(jsonMessage);
-            res.end();
-        }).listen(3000);
+            const receiver = getValueFromJSONFile('email', './config.json');
+            sendEmail(receiver, message);
 
-    });
+            http.createServer(function (req, res) {
+                res.write(jsonMessage);
+                res.end();
+            }).listen(3000);
+
+        });
 });
 
 
-
-
-
-function getCityNameFromFile(fileName) {
-    const text = fs.readFileSync(fileName, 'utf8')
-    const cityObj = JSON.parse(text);
-    const cityName = cityObj.city;
-    return cityName;
+function getValueFromJSONFile(value, fileName) {
+    const data = fs.readFileSync(fileName, 'utf8')
+    const jsonData = JSON.parse(data);
+    if (value === 'city') {
+        const cityName = jsonData.city;
+        return cityName;
+    } else if (value === 'email') {
+        const email = jsonData.email;
+        return email;
+    }
 }
 
 function getInformationAboutLocalTimeAndWeather(localtime, weather) {
     return `Localtime: ${localtime}, Weather: ${weather}`;
+}
+
+function sendEmail(receiver, message) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nellyy.radeva@gmail.com',
+            pass: 'ijtkwodpsdinomha'
+        }
+    });
+
+    var mailOptions = {
+        from: 'nellyy.radeva@gmail.com',
+        to: receiver,
+        subject: 'Localtime & Weather',
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
